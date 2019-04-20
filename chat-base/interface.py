844616e86675fff4a4e2c51e7775d0198fa66a71
Adriana <diddythedodo@gmail.com>
2019-04-20 23:50:14 +0100

    continuação da interface, nao funcional

--- chat-base/interface.py
+++ chat-base/interface.py
@@ -50,16 +50,34 @@ class Application(tk.Frame):
 
         self.quit.pack(side=BOTTOM)
 
+    def cut_message(self, msg):
+        msgArray = msg.split()
+        txt_1 = ""
+        txt_2 = ""
+        n = 0
+        for i in msgArray:
+            if n <= 10:
+                txt_1 += i + " "
+            if n > 10:
+                txt_2 += i + " "
+            n += 1
+        return txt_1, txt_2
+
     # envia a mensagem escrita para o chat
     def send_msgs(self):
         print(format(self.text))
         if self.text.get() != "":
-            self.mensagem_text_box.insert(2, "" + self.text.get())
+            if self.text.get().__sizeof__() >= 20:
+                cut_text = self.cut_message(self.text.get())
+                # cut_text.reverse()
+                self.mensagem_text_box.insert(self.mensagem_text_box.size(), cut_text[0])
+                self.mensagem_text_box.insert(self.mensagem_text_box.size(), cut_text[1])
         self.message.delete(0, 'end')
 
 
 root = tk.Tk()
 root.geometry("1250x680")
-root.resizable(0,0)
+root.resizable(0, 0)
 app = Application(master=root)
 app.mainloop()
+
