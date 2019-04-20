bb16a35949d7d17a369e5d0507d5456b69617b94
Adriana <diddythedodo@gmail.com>
2019-04-20 15:47:43 +0100

    -Text size -Top padding

--- 6-chat-base/interface.py
+++ 6-chat-base/interface.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import tkinter as tk
+import tkinter.font
 
 
 class Application(tk.Frame):
@@ -8,13 +9,16 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
+    # cria os widgets
     def create_widgets(self):
         bot_frame = tk.Frame(self)
         top_frame = tk.Frame(self)
         bot_frame.pack(side=BOTTOM)
         top_frame.pack(side=TOP)
 
-        self.text_box = Listbox(top_frame, width=120, height=30)
+        font = tk.font.Font(size=14)
+
+        self.mensagem_text_box = Listbox(top_frame, width=65, height=25, font=font)
         self.chat_room_text_box = Listbox(top_frame, width=40, height=30)
         self.members_text_box = Listbox(top_frame, width=40, height=30)
 
@@ -26,6 +30,7 @@ class Application(tk.Frame):
                               command=self.master.destroy)
         self.display_widgets()
 
+    # Organiza os widgets usados
     def display_widgets(self):
         self.send_msg["text"] = "Send"
         self.send_msg["command"] = self.send_msgs
@@ -36,16 +41,18 @@ class Application(tk.Frame):
 
         self.message.pack(side=LEFT, padx=10)
 
-        self.text_box.pack(side=LEFT, pady=10, padx=10)
+        self.mensagem_text_box.pack(side=LEFT, pady=10, padx=10)
 
         self.members_text_box.pack(side=RIGHT)
 
         self.quit.pack(side=BOTTOM)
 
+    # envia a mensagem escrita para o chat
     def send_msgs(self):
         print(format(self.text.get()))
         if self.text.get() != "":
-            self.text_box.insert(1, self.text.get())
+            self.mensagem_text_box.insert(1, "")
+            self.mensagem_text_box.insert(2, "   " + self.text.get())
         self.message.delete(0, 'end')
 
 
