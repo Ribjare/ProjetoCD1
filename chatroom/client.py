fb09b97d7cc0012a340d935f2d1e1d9630aa9f5e
Adriana <diddythedodo@gmail.com>
2019-04-21 13:39:47 +0100

    continuação da interface, funcional, ligado ao cliente

--- chatroom/client.py
+++ chatroom/client.py
@@ -4,9 +4,112 @@
 """
 import socket
 import threading
-from chatroom.interface import Application
+from tkinter import*
+import tkinter as tk
+import tkinter.font
 
 isDeactive = False
+
+
+class Client(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    # cria os widgets
+    def create_widgets(self):
+        bot_frame = tk.Frame(self)
+        top_frame = tk.Frame(self)
+        bot_frame.pack(side=BOTTOM)
+        top_frame.pack(side=TOP)
+
+        font = tk.font.Font(size=14)
+
+        self.mensagem_text_box = Listbox(top_frame, width=65, height=25, font=font)
+        self.chat_room_text_box = Listbox(top_frame, width=40, height=30)
+        self.members_text_box = Listbox(top_frame, width=40, height=30)
+
+        self.text = StringVar()
+        self.message = tk.Entry(bot_frame, textvariable=self.text, width=110)
+        self.send_msg = tk.Button(bot_frame)
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                                command=self.master.destroy)
+        self.display_widgets()
+
+    # Organiza os widgets usados
+    def display_widgets(self):
+        self.send_msg["text"] = "Send"
+        self.send_msg["command"] = self.send_msgs
+
+        self.send_msg.pack(side=RIGHT)
+
+        self.chat_room_text_box.pack(side=LEFT)
+
+        self.message.pack(side=LEFT, padx=10)
+
+        self.mensagem_text_box.pack(side=LEFT, pady=10, padx=10)
+
+        self.members_text_box.pack(side=RIGHT)
+
+        self.quit.pack(side=BOTTOM)
+
+        self.add_chatroom_name("")
+        self.add_user("")
+
+    # corta a mensagem, se for demasiado longa
+    def cut_message(self, msg):
+        msgArray = msg.split()
+        txt_1 = ""
+        txt_2 = ""
+        n = 0
+        for i in msgArray:
+            if n <= 10:
+                txt_1 += i + " "
+            if n > 10:
+                txt_2 += i + " "
+            n += 1
+        return txt_1, txt_2
+
+    # envia a mensagem escrita para o chat
+    def send_msgs(self):
+        print(format(self.text))
+        if self.text.get() != "":
+            if not self.text.get().startswith("/"):
+                if self.text.get().__sizeof__() >= 20:
+                    # self.lista_msg.insert(self.lista_msg.__sizeof__(), self.text.get())
+                    cut_text = self.cut_message(self.text.get())
+                    self.mensagem_text_box.insert(self.mensagem_text_box.size(), "[Bob]: " + cut_text[0])
+                    self.mensagem_text_box.insert(self.mensagem_text_box.size(), cut_text[1])
+            if self.text.get().startswith("/"):
+                self.interpret_message()
+        self.message.delete(0, 'end')
+
+    def interpret_message(self):
+        text_split = self.text.get().split()
+        if text_split[0] == "/create":
+            self.add_chatroom_name(text_split[1])
+            print(text_split[1])
+
+    # adiciona um utilizador à lista
+    def add_user(self, name):
+        self.members_text_box.insert(self.members_text_box.size(), "Bob")
+        if name != "":
+            self.members_text_box.insert(self.members_text_box.size(), "#" + name)
+
+    # adiciona um chatroom à lista
+    def add_chatroom_name(self, name):
+        self.chat_room_text_box.insert(0, "#Geral")
+        if name != "":
+            self.chat_room_text_box.insert(self.chat_room_text_box.size(), "#" + name)
+
+
+root = Tk()
+root.geometry("1250x680")
+root.resizable(0, 0)
+app = Client(root)
+app.mainloop()
 
 
 # Receives from the server and handle them
@@ -17,16 +120,6 @@ def handle_msg(client):
         # Read answer
         res = client.recv(1024).decode()
         print(res)
-
-
-Application()
-
-
-def send_message():
-    print("qwer")
-    msg = Application.get_from_list()
-    print(msg)
-    return msg
 
 
 # Define socket host and port
@@ -42,8 +135,8 @@ client_socket.connect((SERVER_HOST, SERVER_PORT))
 # First connection send username
 print("$ Username?")
 # msg = interface.write_message
-# msg = input('> ')
-client_socket.sendall(send_message().encode())
+msg = input('> ')
+client_socket.sendall(msg.encode())
 res = client_socket.recv(1024).decode()
 print(res)
 # Create a thread to receive msg
@@ -54,11 +147,11 @@ thread.start()
 while True:
 
     # Send message
-    # msg = input("")
-    client_socket.sendall(send_message().encode())
+    msg = input("")
+    client_socket.sendall(msg.encode())
 
     # Check for exit
-    if send_message() == '/exit':
+    if msg == '/exit':
         res = client_socket.recv(1024).decode()
         print(res)
         isDeactive = True
@@ -66,3 +159,9 @@ while True:
 
 # Close socket
 client_socket.close()
+
+
+# INTERFACE
+
+
+
